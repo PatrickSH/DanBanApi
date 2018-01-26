b5120c198a6e1348516b79cb053db95750661df1
Patrick Hansen <patrick.hansen@hotmail.com>
2018-01-26 18:59:10 +0100

    Changes

---
+++ app/Http/Controllers/OrganizationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\
+
+class OrganizationController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+}
