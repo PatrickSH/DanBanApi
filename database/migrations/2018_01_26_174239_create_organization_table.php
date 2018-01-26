54d3c399aac4bee4a51cc8faf47bbc2374d623b9
Patrick Hansen <patrick.hansen@hotmail.com>
2018-01-26 18:58:36 +0100

    Changes

---
+++ database/migrations/2018_01_26_174239_create_organization_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("membership_type_id")->unsigned();
+            $table->string("name");
+            $table->tinyInteger("active");
+            $table->foreign('membership_type_id')->references('id')->on('membership_types')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organization');
+    }
+}
---
+++ database/migrations/2018_01_26_174731_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('organization_id')->unsigned();
+            $table->string("username");
+            $table->string("email");
+            $table->string("password");
+            $table->foreign('organization_id')->references('id')->on('organization')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
